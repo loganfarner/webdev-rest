77831d5ca8906d1f5d8aa3c66335889c471ae4f8
Cesar Gonzalez <gonz9682@stthomas.edu>
2023-12-14 14:49:57 -0600

    Added the  input box and 'Go' button for a user to type a location (lat/long coordinates, address, etc.

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,22 @@ import { reactive, ref, onMounted } from 'vue'
 
 let crime_url = ref('');
 let dialog_err = ref(false);
+let searchLocation = ref('');
 let map = reactive(
     {
-        leaflet: null,
-        center: {
-            lat: 44.955139,
-            lng: -93.102222,
-            address: ''
-        },
-        zoom: 12,
-        bounds: {
-            nw: {lat: 45.008206, lng: -93.217977},
-            se: {lat: 44.883658, lng: -92.993787}
-        },
-        neighborhood_markers: [
-            {location: [44.942068, -93.020521], marker: null},
+  leaflet: null,
+  center: {
+    lat: 44.955139,
+    lng: -93.102222,
+    address: ''
+  },
+  zoom: 12,
+  bounds: {
+    nw: {lat: 45.008206, lng: -93.217977},
+    se: {lat: 44.883658, lng: -92.993787}
+  },
+  neighborhood_markers: [
+    {location: [44.942068, -93.020521], marker: null},
             {location: [44.977413, -93.025156], marker: null},
             {location: [44.931244, -93.079578], marker: null},
             {location: [44.956192, -93.060189], marker: null},
@@ -40,7 +41,7 @@ let map = reactive(
 
 // Vue callback for once <template> HTML has been added to web page
 onMounted(() => {
-    // Create Leaflet map (set bounds and valied zoom levels)
+  // Create Leaflet map (set bounds and valied zoom levels)
     map.leaflet = L.map('leafletmap').setView([map.center.lat, map.center.lng], map.zoom);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -70,68 +71,98 @@ onMounted(() => {
 // FUNCTIONS
 // Function called once user has entered REST API URL
 function initializeCrimes() {
-    // TODO: get code and neighborhood data
-    //       get initial 1000 crimes
+  // TODO: get code and neighborhood data
+  //       get initial 1000 crimes
 }
 
 // Function called when user presses 'OK' on dialog box
 function closeDialog() {
-    let dialog = document.getElementById('rest-dialog');
-    let url_input = document.getElementById('dialog-url');
-    if (crime_url.value !== '' && url_input.checkValidity()) {
-        dialog_err.value = false;
-        dialog.close();
-        initializeCrimes();
-    }
+  let dialog = document.getElementById('rest-dialog');
+  let url_input = document.getElementById('dialog-url');
+  if (crime_url.value !== '' && url_input.checkValidity()) {
+    dialog_err.value = false;
+    dialog.close();
+    initializeCrimes();
+  }
     else {
-        dialog_err.value = true;
-    }
+    dialog_err.value = true;
+  }
 }
+
+// Function to update input box text when map is panned/zoomed
+function updateInput() {
+  searchLocation.value = `Lat: ${map.center.lat.toFixed(6)}, Lng: ${map.center.lng.toFixed(6)}`;
+}
+
+// Function to search for location and update the map
+async function searchAndSetLocation() {
+  const apiUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${searchLocation.value}&limit=1`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  if (data.length > 0) {
+    const location = data[0];
+    const lat = parseFloat(location.lat);
+    const lng = parseFloat(location.lon);
+
+    map.center.lat = Math.min(map.bounds.nw.lat, Math.max(map.bounds.se.lat, lat));
+    map.center.lng = Math.min(map.bounds.se.lng, Math.max(map.bounds.nw.lng, lng));
+
+    map.leaflet.setView([map.center.lat, map.center.lng], 16);
+  }
+}
+
 </script>
 
 <template>
     <dialog id="rest-dialog" open>
-        <h1 class="dialog-header">St. Paul Crime REST API</h1>
-        <label class="dialog-label">URL: </label>
-        <input id="dialog-url" class="dialog-input" type="url" v-model="crime_url" placeholder="http://localhost:8000" />
-        <p class="dialog-error" v-if="dialog_err">Error: must enter valid URL</p>
-        <br/>
-        <button class="button" type="button" @click="closeDialog">OK</button>
+      <h1 class="dialog-header">St. Paul Crime REST API</h1>
+      <label class="dialog-label">URL: </label>
+      <input id="dialog-url" class="dialog-input" type="url" v-model="crime_url" placeholder="http://localhost:8000" />
+      <p class="dialog-error" v-if="dialog_err">Error: must enter valid URL</p>
+      <br/>
+      <button class="button" type="button" @click="closeDialog">OK</button>
     </dialog>
     <div class="grid-container ">
-        <div class="grid-x grid-padding-x">
-            <div id="leafletmap" class="cell auto"></div>
-        </div>
-    </div>
-</template>
+      <div class="grid-x grid-padding-x">
+        <div id="leafletmap" class="cell auto"></div>
+      </div>
+      <div class="grid-x grid-padding-x">
+  <label>Enter Location:</label>
+  <input v-model="searchLocation" @change="searchAndSetLocation" />
+  <button class="button" @click="searchAndSetLocation">Go</button>
+</div>
 
+    </div>
+  </template>
+  
 <style>
 #rest-dialog {
-    width: 20rem;
-    margin-top: 1rem;
-    z-index: 1000;
+  width: 20rem;
+  margin-top: 1rem;
+  z-index: 1000;
 }
 
 #leafletmap {
-    height: 500px;
+  height: 500px;
 }
 
 .dialog-header {
-    font-size: 1.2rem;
-    font-weight: bold;
+  font-size: 1.2rem;
+  font-weight: bold;
 }
 
 .dialog-label {
-    font-size: 1rem;
+  font-size: 1rem;
 }
 
 .dialog-input {
-    font-size: 1rem;
-    width: 100%;
+  font-size: 1rem;
+  width: 100%;
 }
 
 .dialog-error {
-    font-size: 1rem;
-    color: #D32323;
+  font-size: 1rem;
+  color: #D32323;
 }
 </style>
